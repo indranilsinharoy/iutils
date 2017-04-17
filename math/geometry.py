adf13cbd6faaeff267eb3fc159705c62cd73c2eb
Indranil Sinharoy <indranil_leo@yahoo.com>
2017-04-17 01:25:33 -0500

    refactored code, added Vector class

--- math/geometry.py
+++ math/geometry.py
@@ -6,8 +6,8 @@
 # Author:        Indranil Sinharoy
 #
 # Created:       06/30/2015
-# Last Modified:
-# Copyright:     (c) Indranil Sinharoy 2015
+# Last Modified: 04/08/2017
+# Copyright:     (c) Indranil Sinharoy 2015 - 2017
 # License:       MIT License
 #-----------------------------------------------------------------------------------------
 """Utility functions for geometry. All examples in docstrings assume that the module has 
@@ -26,10 +26,10 @@ class Point(object):
         self.z = z 
 
     def __repr__(self):
-        return 'Point({}, {}, {})'.format(self.x, self.y, self.z)
+        return '{0.__name__}({1.x!r}, {1.y!r}, {1.z!r})'.format(type(self), self)
 
     def __str__(self):
-        return '({}, {}, {})'.format(self.x, self.y, self.z)
+        return 'Point({0.x!r}, {0.y!r}, {0.z!r})'.format(self)
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y, self.z + other.z)
@@ -44,16 +44,54 @@ class Point(object):
     def __ne__(self, other):
         return not self.__eq__(other)
 
-    def get_dist_from_origin(self):
-        '''returns the Euclidean distance from origin'''
+    @property
+    def dist_from_origin(self):
+        """returns the Euclidean distance from origin"""
         return _np.sqrt(self.x**2 + self.y**2 + self.z**2)
 
     def get_dist_from_point(self, other):
-        '''returns the Euclidean distance from another point'''
+        """returns the Euclidean distance from another point"""
         s = self - other
         return _np.sqrt(s.x**2 + s.y**2 + s.z**2)
 
 
+class Vector:
+    """Three dimensional vector class"""
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return '{0.__name__}({1.x!r}, {1.y!r}, {1.z!r})'.format(type(self), self)
+        
+    def __add__(self, other):
+        """vector addition"""
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+    def __sub__(self, other):
+        """vector substraction"""
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+    def __abs__(self):
+        """absolute value"""
+        return (self.x **2 + self.y **2 + self.z **2) **0.5
+    
+    def __mul__(self, scalar):
+        """scalar multiplication"""
+        return Vector(scalar*self.x, scalar*self.y, scalar*self.z)
+    
+    def __rmul__(self, scalar):
+        return Vector(scalar*self.x, scalar*self.y, scalar*self.z)
+    
+    def __bool__(self):
+        """returns true if vector magnitude is not zero"""
+        return bool(self.x or self.y or self.z)
+    
+    def __getitem__(self, position):
+        """to support indexing and functions such as list(Vector(1, 2, 3))"""
+        return list((self.x, self.y, self.z))[position]
 
 
 #%% Test functions
@@ -67,7 +105,7 @@ def test_Point_class():
     assert isinstance(s, Point)
     _nt.assert_array_equal((p.x + q.x, p.y + q.y, p.z + q.z), (s.x, s.y, s.z))
     # Test methods
-    po = p.get_dist_from_origin()
+    po = p.dist_from_origin
     _nt.assert_almost_equal(po, 5.38516480713)
     pq = p.get_dist_from_point(q)
     qp = q.get_dist_from_point(p)
@@ -82,6 +120,23 @@ def test_Point_class():
     assert p != l
     print('Point class test successful')
 
+def test_Vector_class():
+    vec = Vector()
+    _nt.assert_array_equal((0.0, 0.0, 0.0), (vec.x, vec.y, vec.z))
+    _nt.assert_equal(True, isinstance(vec, Vector), 'Vector instance test failed')
+    vec1 = Vector(1, 2, 3)
+    _nt.assert_equal(True, isinstance(list(vec1), list), 'List instance test failed')
+    _nt.assert_equal(2, vec1[1], 'Vector indexing test failed')
+    vec2 = Vector(4, 5, 6)
+    _nt.assert_array_equal(list((5, 7, 9)), list(vec1 + vec2), 'Vector addition test failed')
+    _nt.assert_array_equal(list((-3, -3, -3)), list(vec1 - vec2), 'Vector substraction test failed')
+    _nt.assert_array_equal(list((2, 4, 6)), list(2*vec1), 'Vector scalar pre-multiplication test failed')
+    _nt.assert_array_equal(list((2, 4, 6)), list(vec1*2), 'Vector scalar post-multiplication test failed')
+    vec3 = Vector(-1, 2, -3)
+    _nt.assert_array_equal(_np.sqrt(vec3[0]**2 + vec3[1]**2 + vec3[2]**2), abs(vec3), 'Vector absolute test failed')
+    _nt.assert_equal(True, bool(vec1), 'Vector bool test 1 failed')
+    _nt.assert_equal(False, bool(vec), 'Vector bool test 2 failed')
+    print('Vector class test successful')
 
 
 if __name__ == '__main__':
@@ -90,3 +145,4 @@ if __name__ == '__main__':
     set_printoptions(precision=4, linewidth=85)  # for visual output in manual tests.
     # test functions
     test_Point_class()
+    test_Vector_class()
